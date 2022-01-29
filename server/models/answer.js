663869f2bfe7bf534be832ff322d855b34fc9b4f
saraiovieira <saravieira09@hotmail.com>
2022-01-29 18:56:06 +0000

    create user route and delete previous ones

--- server/models/answer.js
+++
@@ -1,7 +0,0 @@
-import mongoose from 'mongoose';
-
-const answerSchema = new mongoose.Schema({
-    answer: { type: String, unique: true },
-});
-
-export const Answer = mongoose.model("answer", answerSchema);
