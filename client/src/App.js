d94699601a4aad1638036282d2ce71f323976e91
saraiovieira <saravieira09@hotmail.com>
2022-01-30 12:14:11 +0000

    add login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Question from "./pages/Question";
+import Login from './pages/Login';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Question/>} />
             <Route path="/questions" element={<Question/>} />
+            <Route path="/login" element={<Login/>} />
           </Routes>
     </Router>
   );
---
+++ client/src/pages/Login.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { GoogleLogin } from 'react-google-login';
+import '../components/login.css'
+
+const Login = () => {
+  const navigate = useNavigate()
+  const clientId = '304039575923-obgs1220fde6a1vrgkcfp8c1lmocrfnr.apps.googleusercontent.com';
+
+  const onSuccess = (res) => {
+    navigate(`/`);
+}
+
+const onFailure = (res) => {
+    console.log('[Login failed] res:', res);
+}
+
+return (
+  <>
+  <img src={"/images/logo.png"} />
+    <div className='window'>
+    <GoogleLogin
+      clientId={clientId}
+      buttonText='Login'
+      //onSuccess={onSuccess}
+      onFailure={onFailure}
+      cookiePolicy={'single_host_origin'}
+      isSignedIn={true}
+    />
+    </div>
+  </>
+  );
+};
+
+export default Login;
+
